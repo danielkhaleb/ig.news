93b0f899ad0bd3fc207e4475345656c14b9e4c27
Daniel Khaleb <daniel.khaleb@gmail.com>
2022-03-21 19:49:50 +0000

    feat: add faunadb

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,5 +1,8 @@
+import { query as q } from 'faunadb'
+
 import NextAuth from "next-auth"
 import GithubProvider from "next-auth/providers/github"
+import { fauna } from '../../../services/fauna'
 
 export default NextAuth({
   // Configure one or more authentication providers
@@ -7,8 +10,44 @@ export default NextAuth({
     GithubProvider({
       clientId: process.env.GITHUB_ID,
       clientSecret: process.env.GITHUB_SECRET,
-      
+      authorization: {
+        params: {
+          scope: 'read:user',
+        },
+      },
     }),
     // ...add more providers here
   ],
+  callbacks: {
+    async signIn({ user }) {
+      const { email } = user;
+      try {
+        await fauna.query(
+          q.If(
+            q.Not(
+              q.Exists(
+                q.Match(
+                  q.Index('user_by_email'),
+                  q.Casefold(email)
+                )
+              )
+            ),
+            q.Create(
+              q.Collection('users'),
+              { data: { email } }
+            ),
+            q.Get(
+              q.Match(
+                q.Index('user_by_email'),
+                q.Casefold(email)
+              )
+            )
+          )
+        )
+        return true;
+      } catch {
+        return false
+      }
+    }
+  }
 })
