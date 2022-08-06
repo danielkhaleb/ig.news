702dad81946485304c71c2ba730d779b8a246937
Daniel Khaleb <daniel.khaleb@gmail.com>
2022-08-07 00:24:38 +0100

    refactor: change css and how to get the conten from posts

--- src/pages/posts/[slug].tsx
+++ src/pages/posts/[slug].tsx
@@ -2,12 +2,15 @@ import { GetServerSideProps } from "next"
 import { getSession } from "next-auth/react";
 import Head from "next/head";
 import { createClient } from "../../services/prismic";
+import styles from './post.module.scss'
 
 interface PostProps {
-  slug: string;
-  title: string;
-  content: string;
-  updatedAt: string;
+  post: {
+    slug: string;
+    title: string;
+    content: string;
+    updateAt: string;
+  }
 }
 
 export default function Post({ post } : PostProps) {
@@ -19,8 +22,15 @@ export default function Post({ post } : PostProps) {
         </title>
       </Head>
 
-      <main>
-        
+      <main className={styles.container}>
+        <article className={styles.post}>
+          <h1>{post.title}</h1>
+          <time>{post.updateAt}</time>
+          <div 
+            className={styles.postContent}
+            dangerouslySetInnerHTML={{ __html: post.content }} 
+          />
+        </article>
       </main>
     </>
   )
@@ -31,18 +41,20 @@ export const getServerSideProps: GetServerSideProps = async ({ req, params }) =>
   const { slug } = params;
 
   const client = createClient(req);
-  const response = await client.getByUID('publication', String(slug));
+  const response = await client.getByUID('post', String(slug));
 
   const post = {
     slug,
     title: response.data.title[0].text,
-    content: response.data.Content,
+    content: response.data.Content[0].text,
     updateAt: new Date(response.last_publication_date).toLocaleDateString('pt-BR', {
       day: '2-digit',
       month: 'long',
       year: 'numeric'
     })
   }
+
+  console.log('post', post)
 
   return {
     props: {
